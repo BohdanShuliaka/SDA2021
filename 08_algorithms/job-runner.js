09e57f0232dd41c1e11cc5582bc475a7d12adaf1
bohdanshuliaka <bohdan.shuliaka@gmail.com>
2021-07-05 12:29:43 +0300

    review fixes

--- 08_algorithms/job-runner.js
+++ 08_algorithms/job-runner.js
@@ -21,77 +21,25 @@ class PriorityQueue {
         this.values[index2] = temp;
     }
 
-    bubbleUp() {
-        let index = this.values.length - 1
-        while(index > 0){
-            let parentIndex = Math.floor((index - 1)/2);
-            if(this.values[parentIndex].priority > this.values[index].priority) {
-                this.swap(index, parentIndex);
-                index = parentIndex;
-            } else{
+    enqueue(qElement) {
+        var contain = false;
+    
+        for (var i = 0; i < this.values.length; i++) {
+            if (this.values[i].priority > qElement.priority) {
+                this.values.splice(i, 0, qElement);
+                contain = true;
                 break;
             }
         }
-        return 0;
-    }
+        if (!contain) {
+            this.values.push(qElement);
+        }
 
-    enqueue(value) {
-        this.values.push(value)
-        this.bubbleUp();
-        return this.values
-    }
-
-    bubbleDown() {
-        let parentIndex = 0;
-        const length = this.values.length;
-        const elementPriority = this.values[0].priority;
-
-        while (true) {
-            //get indexes of child elements by following formula
-            let leftChildIndex = (2 * parentIndex) + 1;
-            let rightChildIndex = (2 * parentIndex) + 2;
-            let leftChildPriority, rightChildPriority;
-            let indexToSwap = null;
-            // if left child exists, and is greater than the element, plan to swap with the left child index
-            if(leftChildIndex < length) {
-                leftChildPriority = this.values[leftChildIndex].priority
-                if(leftChildPriority < elementPriority){
-                    indexToSwap = leftChildIndex;
-                }
-            }
-            //if right child exists
-            if(rightChildIndex < length) {
-                rightChildPriority = this.values[rightChildIndex].priority
-
-                if(
-                    //if right child is greater than element and there are no plans to swap
-                    (rightChildPriority < elementPriority && indexToSwap === null) ||
-                    //OR if right child is greater than left child and there ARE plans to swap
-                    (rightChildPriority < leftChildPriority && indexToSwap !== null))
-                {
-                    //plan to swap with the right child
-                    indexToSwap = rightChildIndex
-                }
-            }
-            //if there are no plans to swap, break out of the loop
-            if(indexToSwap === null) {
-                break;
-            } 
-            //swap with planned element
-            this.swap(parentIndex, indexToSwap);
-            //starting index is now index that we swapped with
-            parentIndex = indexToSwap;
-        }  
+        return this.values;
     }
 
     dequeue() {
-        this.swap(0, this.values.length - 1);
-        let poppedNode = this.values.pop();
-        if(this.values.length > 1){
-            this.bubbleDown();
-        }
-        
-        return poppedNode;
+        return this.values.length === 0 ? "Underflow" : this.values.shift();
     }
 }
 
